7eb89e164171a87e93a4c9680a12234e93c0e261
ssmilesquare <ssmilesquare@gmail.com>
2021-08-08 22:43:51 +0700

    T______T

--- src/App.tsx
+++ src/App.tsx
@@ -1,64 +1,22 @@
 import React from 'react';
-import { useState } from 'react'
-import header from './component/header';
+import Header from './component/Header';
+import Footer from './component/Footer';
+import Todo from './component/Todo';
+import { useState } from 'react';
+
 
 function App() {
-
-  const onKeyDownCallback = (ev: React.KeyboardEvent<HTMLInputElement>) => {
-    /* check pressing enter key here */
-  }
 
   return (
     <div>
       {/* header section */}
-      <header/>
-      {/* <div className='flex justify-center items-end space-x-2'>
-        <span className='text-center italic my-2 text-2xl'>Minimal Todo List </span>
-        <span className='text-gray-400 italic my-2 text-xl'>by ...</span>
-      </div> */}
+      <Header></Header>
 
       {/* todo section */}
-      <todo/>
-      {/* <div className='mx-auto max-w-4xl'> */}
-
-        {/* task input and add button */}
-        {/* <div className='flex space-x-1'>
-          <input className='border border-gray-400 w-full text-2xl'
-            onKeyDown={onKeyDownCallback} ></input>
-          <button className='border border-gray-400 w-8 font-bold'>+</button>
-        </div> */}
-
-        {/* tasks section */}
-        {/* <div> */}
-          {/* task example */}
-          {/* Please convert this into a task component */}
-          {/* <div
-            className="flex justify-between h-8 items-center py-6 border-b"
-          >
-            <span className="text-2xl"> I am a task </span>
-            <div className="flex space-x-1 items-center">
-              <button className="bg-green-400 w-24 text-2xl" >Done</button>
-              <button className="bg-red-400 w-24 text-2xl" >Delete</button>
-            </div>
-          </div> */}
-
-          {/* another task example */}
-          {/* <div
-            className="flex justify-between h-8 items-center py-6 border-b"
-          >
-            <span className="text-2xl"> I am another task </span>
-            <div className="flex space-x-1 items-center">
-              <button className="bg-green-400 w-24 text-2xl" >Done</button>
-              <button className="bg-red-400 w-24 text-2xl" >Delete</button>
-            </div>
-          </div>
-
-        </div>
-      </div> */}
-
+      <Todo></Todo>
+      
       {/* footer section */}
-      <footer/>
-      {/* <p className='text-center text-gray-400'> ... </p> */}
+      <Footer></Footer>
     </div>
   );
 }
