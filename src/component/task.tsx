79b80f136462cf7c15d9e555f94fad3e0cf3c027
ssmilesquare <ssmilesquare@gmail.com>
2021-08-09 15:07:39 +0700

    completed

--- src/component/task.tsx
+++ src/component/task.tsx
@@ -8,50 +8,16 @@ type TaskProps = {
     statusDone: Boolean
 }
 
-const Task = ({ id, text, doneFn, deleteFn, statusDone }: TaskProps) => {
-    const [mouse_Enter, setStatus] = useState<boolean>(false)
-
-    const mouseEnter = () => {
-        setStatus(true)
-    }
-
-    const mouseLeave = () => {
-        setStatus(false)
-    }
-
-
-    if (!statusDone && mouse_Enter) {
-        return (
-            <div>
-                {/* tasks section */}
-                <div className="flex justify-between h-8 items-center py-6 border-b" onMouseEnter={mouseEnter} onMouseLeave={mouseLeave}>
-                    <span className="text-2xl"> {text} </span>
-                    <div className="flex space-x-1 items-center">
-                        <button className="bg-green-400 w-24 text-2xl" onClick={() => doneFn(id)}>Done</button>
-                        <button className="bg-red-400 w-24 text-2xl" onClick={() => deleteFn(id)}>Delete</button>   
-                    </div>
-                </div>
-            </div>)
-    }else if(!statusDone && !mouse_Enter){
-        return (
-            <div>
-                {/* tasks section */}
-                <div className="flex justify-between h-8 items-center py-6 border-b" onMouseEnter={mouseEnter} onMouseLeave={mouseLeave}>
-                    <span className="text-2xl"> {text} </span>
-                    <div className="flex space-x-1 items-center"></div>
-                </div>
-            </div>)
-    } else {
-        return (<div>
-            {/* tasks section */}
-            <div>
-                <div className="flex justify-between h-8 items-center py-6 border-b">
-                    <span className="text-2xl line-through"> {text} </span>
-                </div>
+const Task = ({ id, name, doneFn, deleteFn }: TaskProps) => {
+    const [Mouse, setIsMouseInside] = useState<boolean>(false);
+    return (
+        <div className="flex justify-between h-8 items-center py-6 border-b" onMouseEnter={() => setIsMouseInside(true)} onMouseLeave={() => setIsMouseInside(false)}>
+            <span className="text-2xl"> {name}</span>
+            <div className="flex space-x-1 items-center" style={(Mouse) ? { visibility: "visible" } : { visibility: "hidden" }}>
+                <button className="bg-green-400 w-24 text-2xl" onClick={() => doneFn(id)} >Done</button>
+                <button className="bg-red-400 w-24 text-2xl" onClick={() => deleteFn(id)}>Delete</button>
             </div>
-        </div>)
-    }
-
-
+        </div>
+    )
 }
 export default Task
