b3bd1572f1a01c566a9483fd9be8301fde6cd568
ssmilesquare <ssmilesquare@gmail.com>
2021-08-09 15:05:59 +0700

    completed

--- src/component/todo.tsx
+++ src/component/todo.tsx
@@ -8,54 +8,65 @@ type TaskData = {
 }
 
 const Todo = () => {
-    const [input,setInputValue] = useState<string>("")
-    const [todoList,setTodoList] = useState<TaskData[]>([])
-    const [doneList,setDoneList] = useState<TaskData[]>([])
-
-    const onKeyDownCallback = (ev: React.KeyboardEvent<HTMLInputElement>) => {
-        /* check pressing enter key here */
-        if (ev.key === "Enter") addList()
-    }
+    const [curTask, setCurTask] = useState<string>('')
+    const [tasks, setTasks] = useState<TaskData[]>([])
+    const [donetasks, setdoneTasks] = useState<TaskData[]>([])
 
     const onChangeCallback = (ev: React.ChangeEvent<HTMLInputElement>) => {
-        setInputValue(ev.target.value)
-      }
+        setCurTask(ev.target.value)
+    }
 
-    const addList = () => {
-        if (input === "") {
-            alert("Task connot be empty!!")
-        }else{
-            const Todo = todoList
-            const Id = (new Date()).getTime()
-            Todo.push({id:Id,text:input})
-            setTodoList(Todo)
-            setInputValue("")  
+    const onKeyDownCallback = (ev: React.KeyboardEvent<HTMLInputElement>) => {
+        if (ev.key == 'Enter')
+            addTask(curTask)
+    }
+
+    const reset = () => {
+        const re = curTask;
+        const reValue = document.querySelector('input');
+        if (reValue != null) {
+            reValue.value = "";
         }
-        document.querySelectorAll("input")[0].value = ""
-    }
-    const addDoneList = (Id: number) => {
-        const Done = doneList
-        const doneTask = todoList.filter(x => x.id === Id)
-        const temp = doneTask.concat(Done)
-        setDoneList(temp)
-
-        setTodoList(todoList.filter(x => x.id !== Id))
+        setCurTask("")
     }
 
-    const addDeleteList = (Id : number) => {
-        setTodoList(todoList.filter(x => x.id !== Id))
+    const addTask = (taskName: string) => {
+        if (taskName == "") {
+            alert("Task cannot be empty");
+        } else {
+            const newId = (new Date()).getTime()
+            const newTasks = [{ id: newId, name: taskName }, ...tasks]
+            setTasks(newTasks)
+        }
+        reset();
+    }
+
+    const deleteTask = (id: number) => {
+        const newTasks = tasks.filter(x => x.id !== id)
+        setTasks(newTasks)
+    }
+
+    const doneTask = (id: number) => {
+        const newDone = tasks
+        const task = newDone[newDone.findIndex(x => x.id === id)];
+        const newId = (new Date()).getTime()
+        const newdoneTasks = [{ id: newId, name: task.name }, ...donetasks]
+        setdoneTasks(newdoneTasks)
+        const newTasks = tasks.filter(x => x.id !== id)
+        setTasks(newTasks)
     }
 
     return (
         <div className='mx-auto max-w-4xl'>
-            {/* task input and add button */}
             <div className='flex space-x-1'>
                 <input className='border border-gray-400 w-full text-2xl'
                     onKeyDown={onKeyDownCallback} onChange={onChangeCallback}></input>
-                <button className='border border-gray-400 w-8 font-bold' onClick={addList}>+</button>
+                <button className='border border-gray-400 w-8 font-bold' onClick={() => addTask(curTask)}>+</button>
             </div>
-            {todoList.map( x => <Task id={x.id} text={x.text} doneFn={addDoneList} deleteFn={addDeleteList} statusDone={false}/>).reverse()}
-            {doneList.map( x => <Task id={x.id} text={x.text} doneFn={addDoneList} deleteFn={addDeleteList} statusDone={true}/>)}
+
+                {tasks.map(x => <Task id={x.id} name={x.name} doneFn={doneTask} deleteFn={deleteTask} />)}
+            
+                {donetasks.map(x => <Done id={x.id} name={x.name} />)}
         </div>
     )
 }
